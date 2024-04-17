49617cef15e3274b8d0cfe9c1e692f63a228e6d6
jyoti7892 <jyotipatil9242@gmail.com>
2024-04-17 13:17:02 +0530

    first commite

---
+++ script.js
@@ -0,0 +1,42 @@
+
+const hover=document.querySelector('.service');
+const shownavbar=document.querySelector('.second-nav');
+
+let c=1;
+hover.addEventListener('click',()=>{
+
+    if(c===1)
+    {
+        shownavbar.style.display="flex";
+        c--;
+    }
+    else{
+        shownavbar.style.display="none";
+        c++;
+    }
+
+})
+
+
+// const nav=document.querySelector('.first-nav')
+const nav=document.querySelector('.first-nav');
+const open=document.querySelector(".open");
+const close=document.querySelector(".close");
+
+open.addEventListener('click', ()=>{
+nav.style.top="-34px";
+close.style.display="inline-block";
+})
+
+close.addEventListener('click', ()=>{
+nav.style.top="-300px";
+close.style.display="none";
+shownavbar.style.display="none";
+
+})
+
+// footer
+// const BTN=document.querySelector(".btn");
+// BTN.addEventListener("click",()=>{
+//     window.URL(ContactUs.html);
+// })
