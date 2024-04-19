30410981fbfc7e62ffee4f65630d041521087a1e
jyoti7892 <jyotipatil9242@gmail.com>
2024-04-19 14:24:35 +0530

    Made-Responsive

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
+const hover = document.querySelector(".service");
+const shownavbar = document.querySelector(".second-nav");
 
-const hover=document.querySelector('.service');
-const shownavbar=document.querySelector('.second-nav');
+let c = 1;
+hover.addEventListener("click", () => {
+  if (c === 1) {
+    shownavbar.style.display = "flex";
+    c--;
+  } else {
+    shownavbar.style.display = "none";
+    c++;
+  }
+});
 
-let c=1;
-hover.addEventListener('click',()=>{
+const nav = document.querySelector(".first-nav");
+const open = document.querySelector(".open");
+const close = document.querySelector(".close");
 
-    if(c===1)
-    {
-        shownavbar.style.display="flex";
-        c--;
-    }
-    else{
-        shownavbar.style.display="none";
-        c++;
-    }
+open.addEventListener("click", () => {
+  nav.style.top = "-34px";
+  close.style.display = "inline-block";
+});
 
-})
-
-
-const nav=document.querySelector('.first-nav');
-const open=document.querySelector(".open");
-const close=document.querySelector(".close");
-
-open.addEventListener('click', ()=>{
-nav.style.top="-34px";
-close.style.display="inline-block";
-})
-
-close.addEventListener('click', ()=>{
-nav.style.top="-300px";
-close.style.display="none";
-shownavbar.style.display="none";
-
-})
+close.addEventListener("click", () => {
+  nav.style.top = "-300px";
+  close.style.display = "none";
+  shownavbar.style.display = "none";
+});
