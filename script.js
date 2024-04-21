1d1c1a4374aafa91f5e86ccf1dc57b1c77914a68
jyoti7892 <jyotipatil9242@gmail.com>
2024-04-21 18:15:09 +0530

    deploy

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const hover = document.querySelector(".service");
-const shownavbar = document.querySelector(".second-nav");
+// const hover = document.querySelector(".service");
+// const shownavbar = document.querySelector(".second-nav");
 
-let c = 1;
-hover.addEventListener("click", () => {
-  if (c === 1) {
-    shownavbar.style.display = "flex";
-    c--;
-  } else {
-    shownavbar.style.display = "none";
-    c++;
-  }
-});
+// let c = 1;
+// hover.addEventListener("click", () => {
+//   if (c === 1) {
+//     shownavbar.style.display = "flex";
+//     c--;
+//   } else {
+//     shownavbar.style.display = "none";
+//     c++;
+//   }
+// });
 
 const nav = document.querySelector(".first-nav");
 const open = document.querySelector(".open");
@@ -24,5 +24,20 @@ open.addEventListener("click", () => {
 close.addEventListener("click", () => {
   nav.style.top = "-300px";
   close.style.display = "none";
-  shownavbar.style.display = "none";
+  // shownavbar.style.display = "none";
+});
+
+
+const nav1 = document.querySelector(".second-nav");
+const open_service = document.querySelector(".service-open");
+const close_service = document.querySelector(".service-close");
+
+open_service.addEventListener("click", () => {
+  nav1.style.top = "-55px";
+  close_service.style.display = "inline-block";
+});
+
+close_service.addEventListener("click", () => {
+  nav1.style.top= "-300px";
+  close_service.style.display = "none";
 });
